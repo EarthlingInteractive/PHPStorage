34334a8024c0838e058eadd0e8477c26c914b4e3
Dan Stevens <stevens@earthit.com>
2015-10-23 14:58:45 -0500

    Assert expected user count in StorageTest#testInsertSimple.

--- test/EarthIT/Storage/StorageTest.php
+++ test/EarthIT/Storage/StorageTest.php
@@ -7,9 +7,16 @@ class EarthIT_Storage_StorageTest extends EarthIT_Storage_TestCase
 		$entityId0 = $this->registry->storageHelper->newEntityId();
 		$entityId1 = $this->registry->storageHelper->newEntityId();
 		$userRc = $this->registry->schema->getResourceClass('user');
+		
+		$oldUserCount = $this->registry->storageHelper->queryValue("SELECT COUNT(*) FROM storagetest.user");
+		
 		$this->registry->postgresStorage->saveItems( array(
 			array('ID' => $entityId0, 'username' => 'Bob Hope', 'password' => 'asd123'),
 			array('ID' => $entityId1, 'username' => 'Bob Jones', 'password' => 'asd125'),
 		), $userRc);
+		
+		$newUserCount = $this->registry->storageHelper->queryValue("SELECT COUNT(*) FROM storagetest.user");
+		
+		$this->assertEquals( $oldUserCount + 2, $newUserCount );
 	}
 }
